53292dfa2d63cb29d34fe159b1ca3585c9a31754
Erick Andrés Obregón Fonseca <erickobregonf@gmail.com>
2020-08-26 00:01:03 -0600

    [Update] - Variables

---
+++ 02-variables/variables.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+/*
+ * data_type                size    range
+ * 
+ * short int                2	    -32,768 to 32,767
+ * unsigned short int	    2	    0 to 65,535
+ * unsigned int	            4	    0 to 4,294,967,295
+ * int	                    4	    -2,147,483,648 to 2,147,483,647
+ * long int	                8	    -2,147,483,648 to 2,147,483,647
+ * unsigned long int	    4	    0 to 4,294,967,295
+ * long long int	        8	    -(2^63) to (2^63)-1
+ * unsigned long long int	8	    0 to 18,446,744,073,709,551,615
+ * signed char	            1	    -128 to 127
+ * unsigned char	        1	    0 to 255
+ * float	                4
+ * double	                8	
+ * long double	            12
+ */
+
+
+int main(int argc, char const *argv[])
+{
+    // Integer
+    int int_number = 10;
+    // Character
+    char character = 'h';
+    // Boolean
+    bool boolean = true; // false
+    // Floating Point
+    float float_number = 10.0;
+    // Double Floating Point
+    double double_number = 12e12;
+
+    return 0;
+}
